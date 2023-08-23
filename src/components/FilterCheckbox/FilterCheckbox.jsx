4c22d1c343c8defe9163db0e5d06e0948601421b
Maria Troyanovskaya <lamarilu@ya.ru>
2023-08-23 23:20:09 +0300

    исправление замечаний #6: не так поняла замечание о том, что чекбокс нужно вложить в тег <form>, в SearchForm он уже был вложен

--- src/components/FilterCheckbox/FilterCheckbox.jsx
+++ src/components/FilterCheckbox/FilterCheckbox.jsx
@@ -3,13 +3,11 @@ import "./FilterCheckbox.css";
 
 function FilterCheckbox() {
   return (
-    <form>
       <label htmlFor="shortfilms" className="filter-checkbox">
         <input className="filter-checkbox__input" type="checkbox" id="shortfilms" />
         <span className="filter-checkbox__span elements" />
         Короткометражки
       </label>
-    </form>
   );
 }
 
