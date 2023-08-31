c79b9d31c81dbeab4dca628e7bd85f7cb99980e5
Maria Troyanovskaya <lamarilu@ya.ru>
2023-08-31 20:40:57 +0300

    base_url

--- src/utils/MainApi.js
+++ src/utils/MainApi.js
@@ -1,5 +1,5 @@
-//export const BASE_URL = 'https://api.lamarilumovies.nomoredomains.xyz';
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = 'https://api.lamarilumovies.nomoredomains.xyz';
+//export const BASE_URL = 'http://localhost:3000';
 
 const handleResponse = (res) => {
   if (res.ok) {
